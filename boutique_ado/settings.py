bb1068607d616ea00734b5846ae85ed036f47b7a
Dee Torna <tornadavinder@gmail.com>
2024-02-27 11:36:33 +0000

    updated context processor

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-deesingh92-boutique-lx3atz5d2bg.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-deesingh92-boutique-zhhsensc5p5.ws-eu108.gitpod.io']
 
 
 # Application definition

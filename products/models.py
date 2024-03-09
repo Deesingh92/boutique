680819dff86ed495a443f9de304002c5bc7649fc
Dee Torna <tornadavinder@gmail.com>
2024-03-09 12:50:03 +0000

    finished add product functionality

--- products/models.py
+++ products/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Catergories'
+        verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -11,8 +12,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_friendly(self):
+    def get_friendly_name(self):
         return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
@@ -26,4 +28,4 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
